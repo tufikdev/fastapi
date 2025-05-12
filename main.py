6416b53c547d11d9553c7d5792682d49f5177a1e
tufikdev <tufikdev@gmail.com>
2025-05-11 23:01:46 -0600

    Update main.py
    Se actualizo el api con nuevo enpoint para probar

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/items/")
+def read_item_all(q: Optional[str] = None):
+    return {"items": [{"item_id","item01"},{"item_id","item02"},{"item_id","item03"}], "q": q}
